6815513c61fdb4ba59f12a63875c4fec6aa0ff7d
MaximillionSnyder <stanyed73@gmail.com>
2026-07-26 23:40:41 -0500

    chore: bump version a 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = (project.findProperty("versionName") as String?) ?: "0.3"
+        versionName = (project.findProperty("versionName") as String?) ?: "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
